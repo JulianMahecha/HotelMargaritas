3fa2c2bdba78447a8d0f6464e3fd33acde57f797
Julian Mahecha <jemahechaj@ut.edu.co>
2019-02-20 20:03:55 -0500

    Version Final del Proyecto

--- Group5/NearbyHoosierHotels..xaml.cs
+++ Group5/NearbyHoosierHotels..xaml.cs
@@ -36,35 +36,52 @@ namespace Group5
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
 
-            if (cboZipCode.SelectedIndex == 1)
+            if (cboZipCode.SelectedIndex == 0)
             {
-                txtOutput.Text = "===============================================================" + Environment.NewLine + "==================== HOTELES LAS MARGARITAS ==================="
-                + Environment.NewLine + "===============================================================" + Environment.NewLine + "Direccion".PadRight(22) + "Telefono".PadRight(12) + "Movil".PadRight(20)
-                + "Barrio".PadRight(16) + Environment.NewLine + "Calle 46 No. 23b-27".PadRight(22) + "7141527".PadRight(12) + "317-789-8989".PadRight(20)
-                    + "Rosales".PadRight(16) + Environment.NewLine + "Av 68 N. 23-42".PadRight(22) + "2043227".PadRight(12) + "317-456-2154".PadRight(20)
-                    + "Chapinero".PadRight(16) + Environment.NewLine + "Calle 127 No 12-65".PadRight(22) + "9154782".PadRight(12) + "812-617-2200".PadRight(20)
-                    + "Candelaria".PadRight(16) + Environment.NewLine + "Calle 102 No. 25-23".PadRight(22) + "2030506".PadRight(12) + "812-541-7100".PadRight(20)
-                    + "Chico".PadRight(16) + Environment.NewLine;
+                txtOutput.Text = "===============================================================" + Environment.NewLine + "================ HOTELES LAS MARGARITAS ONE KING =============="
+                + Environment.NewLine + "===============================================================" + Environment.NewLine
+                + "Cama sencilla con colchon ortopedico".PadRight(16) + Environment.NewLine + "Servicio de tv por cable".PadRight(16) + Environment.NewLine + "WiFi 5".PadRight(16) 
+                + Environment.NewLine;
+            }
+            else if (cboZipCode.SelectedIndex == 1)
+            {
+                txtOutput.Text = "===============================================================" + Environment.NewLine + "============= HOTELES LAS MARGARITAS ONE KING DELUXE ============"
+                + Environment.NewLine + "===============================================================" + Environment.NewLine
+                + "Cama doble con colchon ortopedico".PadRight(16) + Environment.NewLine + "Servicio de tv por Satelital".PadRight(16) + Environment.NewLine + "WiFi 5".PadRight(16)
+                + Environment.NewLine + "Tina de Baño".PadRight(16)+ Environment.NewLine + "Esencias Para Baño".PadRight(16);
             }
             else if (cboZipCode.SelectedIndex == 2)
             {
-                txtOutput.Text = "===============================================================" + Environment.NewLine + "==================== HOTELES LAS MARGARITAS ==================="
-                + Environment.NewLine + "===============================================================" + Environment.NewLine + "Direccion".PadRight(22) + "Telefono".PadRight(12) + "Movil".PadRight(20)
-                + "Barrio".PadRight(16) + Environment.NewLine + "Calle 50 No. 23b-27".PadRight(22) + "2014569".PadRight(12) + "317-789-9999".PadRight(20)
-                    + "El Poblado".PadRight(16) + Environment.NewLine + "Av 68 N. 23-42".PadRight(22) + "2045227".PadRight(12) + "317-456-1818".PadRight(20)
-                    + "Uribia".PadRight(16) + Environment.NewLine + "Av 25 No 23-89".PadRight(22) + "7142369".PadRight(12) + "812-617-2200".PadRight(20)
-                    + "La Palma".PadRight(16) + Environment.NewLine + "Calle 10 No. 25-23".PadRight(22) + "2030506".PadRight(12) + "812-541-2320".PadRight(20)
-                    + "Andurez".PadRight(16) + Environment.NewLine;
+                txtOutput.Text = "===============================================================" + Environment.NewLine + "============= HOTELES LAS MARGARITAS TWO QUEENS ============"
+                + Environment.NewLine + "===============================================================" + Environment.NewLine
+                + "Cama doble con colchon ortopedico Antialergenico".PadRight(16) + Environment.NewLine + "Servicio de tv por Satelital".PadRight(16) + Environment.NewLine + "WiFi 15MB".PadRight(16)
+                + Environment.NewLine + "Jacuzzi".PadRight(16) + Environment.NewLine + "Esencias Para Baño".PadRight(16) + Environment.NewLine + "Netflix HD".PadRight(16);
             }
             else if (cboZipCode.SelectedIndex == 3)
             {
-                txtOutput.Text = "===============================================================" + Environment.NewLine + "==================== HOTELES LAS MARGARITAS ==================="
-                + Environment.NewLine + "===============================================================" + Environment.NewLine + "Direccion".PadRight(22) + "Telefono".PadRight(12) + "Movil".PadRight(20)
-                + "Barrio".PadRight(16) + Environment.NewLine + "Carrera 7, nº 27-42".PadRight(22) + "2068974".PadRight(12) + "317-789-8989".PadRight(20)
-                    + "La Granja".PadRight(16) + Environment.NewLine + "Dg 21 No. 52-69".PadRight(22) + "7156987".PadRight(12) + "317-456-1874".PadRight(20)
-                    + "San Blas".PadRight(16) + Environment.NewLine + "Km2 Via Sta Marta".PadRight(22) + "2365410".PadRight(12) + "812-617-2330".PadRight(20)
-                    + "Playa Blanca".PadRight(16) + Environment.NewLine + "Cra 28 no. 26-27".PadRight(22) + "9080203".PadRight(12) + "812-541-3640".PadRight(20)
-                    + "Andesino".PadRight(16) + Environment.NewLine;
+                txtOutput.Text = "===============================================================" + Environment.NewLine + "=========== HOTELES LAS MARGARITAS TWO QUEENS DELUXE ==========="
+                + Environment.NewLine + "===============================================================" + Environment.NewLine
+                + "Cama King Size con colchon ortopedico Antialergenico".PadRight(16) + Environment.NewLine + "Servicio de tv Satelital".PadRight(16) + Environment.NewLine + "WiFi 15MB".PadRight(16)
+                + Environment.NewLine + "Jacuzzi".PadRight(16) + Environment.NewLine + "Esencias Para Baño".PadRight(16) + Environment.NewLine + "Netflix UHD".PadRight(16) + Environment.NewLine 
+                + "Cinta de Correr".PadRight(16) + "Servicio Dog Friendly".PadRight(16);
+            }
+            else if (cboZipCode.SelectedIndex == 4)
+            {
+                txtOutput.Text = "===============================================================" + Environment.NewLine + "=========== HOTELES LAS MARGARITAS ONE KING SUITE ==========="
+                + Environment.NewLine + "===============================================================" + Environment.NewLine
+                + "Cama King Size con colchon ortopedico Antialergenico".PadRight(16) + Environment.NewLine + "Servicio de tv Satelital".PadRight(16) + Environment.NewLine + "WiFi 45MB".PadRight(16)
+                + Environment.NewLine + "Jacuzzi".PadRight(16) +Environment.NewLine+ "Esencias Para Baño".PadRight(16) + Environment.NewLine+ "Netflix UHD & HULU".PadRight(16) + Environment.NewLine + "Cinta de Correr".PadRight(16)
+                + Environment.NewLine + "Servicio Dog Friendly".PadRight(16) + Environment.NewLine + "Servicio Dog Friendly".PadRight(16) + Environment.NewLine + "Nevera Inroom".PadRight(16) + Environment.NewLine + 
+                "Vista Preferencial".PadRight(16);
+            }
+            else if (cboZipCode.SelectedIndex == 5)
+            {
+                txtOutput.Text = "===============================================================" + Environment.NewLine + "=========== HOTELES LAS MARGARITAS ONE KING PRESIDENTIAL SUITE ==========="
+                + Environment.NewLine + "===============================================================" + Environment.NewLine
+                + "Cama King Size con colchon ortopedico Antialergenico, Espacio Amoblado tipo galeria y Chimenea".PadRight(16) + Environment.NewLine + "Servicio de tv Satelital".PadRight(16) + Environment.NewLine + "WiFi 200MB".PadRight(16)
+                + Environment.NewLine + "Jacuzzi y Sauna Indoor".PadRight(16) + " , Esencias Para Baño".PadRight(16) + Environment.NewLine + "Netflix UHD, HULU y HBO".PadRight(16) + Environment.NewLine + "Cinta de Correr".PadRight(16)
+                + Environment.NewLine + "Servicio Dog Friendly".PadRight(16) + Environment.NewLine + "Servicio Dog Friendly".PadRight(16) + Environment.NewLine + "Nevera Inroom".PadRight(16) + Environment.NewLine +
+                "Vista Preferencial con Balcon".PadRight(16);
             }
             else
             {
